a70998a785cd20597f76eb4313a77e5144bd2468
aandersonozhoh <89994451+aandersonozhoh@users.noreply.github.com>
2022-03-30 17:29:18 -0500

    Test

--- endScript.js
+++ endScript.js
@@ -2,6 +2,7 @@ const generateButton = document.querySelector('button')
 const refreshButton = document.querySelector('#Refresh')
 const adviceSpot = document.querySelector('p')
 const imageDiv = document.querySelector('div')
+const howTo = document.querySelector('h1')
 
 // on click, the person's image appears in the div and the advice appears in the 'p'
 
@@ -11,11 +12,11 @@ generateButton.addEventListener('click', async () => {
     console.log(response.data.slip.advice)
     let adviceLine = response.data.slip.advice
     adviceSpot.innerHTML = `${adviceLine}`
+    howTo.innerText = `Note: for advice from someone else, click "Someone Else" and click "Advice" again`
 
 })
 refreshButton.addEventListener('click',() =>{
     location.reload()
-    imageDiv.innerHTML =`<img src = https://thispersondoesnotexist.com/image? width =\'500px\' height =\'500\'>`
 })
 
 
